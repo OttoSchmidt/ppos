8637d11249048114cb68c22a77bc074fa6dbbe4b
Otto Schmidt <ottoschmidt23@gmail.com>
2026-05-27 15:28:52 -0300

    [fix] importacoes

--- kernel/mqueue.c
+++ kernel/mqueue.c
@@ -1,10 +1,13 @@
 // PingPongOS - PingPong Operating System
 // GRR20244386 - Otto Schmidt
 // GRR20244625 - Vinícius Hasse Nascimento
+#include <stdlib.h>
+#include <string.h>
 
 #include "mqueue.h"
 #include "semaphore.h"
-#include "libc.h"
+#include "lib/libc.h"
+
 
 struct mqueue_t {
     void *buffer;                   // buffer circular
